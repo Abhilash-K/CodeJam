c124d6e6b26e692cc111fc25ba128e8547765b3e
Abhilash K <abhilash.face@gmail.com>
2017-03-26 17:08:11 +0530

    Sample done

--- BasicInputOutput/process.js
+++ BasicInputOutput/process.js
@@ -4,20 +4,27 @@
 function takeRawinput(text)
 {
     var splitArray = [];
-    splitArray = text.split('\n');
-    var outputField = document.getElementById("OutputText");
+    var solnArray = [];
     var outputTextString = "";
-    for(var i=0;i<splitArray.length;i++)
+    splitArray = text.split('\n');
+    outputTextString = findSolution(splitArray);
+    solnArray = outputTextString.split('\n');
+    var outputField = document.getElementById("OutputText");
+
+    var sizeOfTextArea = (solnArray.length) * 20;
+    /*for(var i=0;i<solnArray.length;i++)
     {
         if(outputTextString == "")
         {
-            outputTextString += splitArray[i];
+            outputTextString += solnArray[i];
         }
         else
         {
-            outputTextString += '\n' + splitArray[i];
+            outputTextString += '\n' + solnArray[i];
         }
-    }
+    }*/
     outputField.value = outputTextString;
-    outputField.setAttribute("height", "100px");
+    outputField.style.height = String(sizeOfTextArea)+"px";
+
+
 }
