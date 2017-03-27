9b575ea997de43451281d96127942f8a81e14dd8
Abhilash K <abhilash.face@gmail.com>
2017-03-27 23:51:10 +0530

    Started Coin Jam
    Solution to https://code.google.com/codejam/contest/6254486/dashboard

---
+++ Coin Jam/solution.js
@@ -0,0 +1,56 @@
+/**
+ * Created by Abhilash K on 3/26/2017.
+ */
+
+function findSolution(inputArray)
+{
+    var numtestCases = inputArray[0];
+    var solutionString = "";
+
+    for(var i=1;i<=numtestCases;i++)
+    {
+        S[i-1] = inputArray[i];
+    }
+    for(var j=0; j<S.length;j++)
+    {
+        solutionString += "Case #" + String(j +1) + ": ";
+
+        
+
+        if((j+1) != S.length)
+        {
+            solutionString += "\n";
+        }
+    }
+    return solutionString;
+}
+
+function findSolutionOld(inputArray)
+{
+    var numtestCases = inputArray[0];
+    var numCount = [];
+    var numSymbols = [];
+    var splitLineArray = [];
+    var solutionString = "";
+    for(var i=1;i<=numtestCases;i++)
+    {
+        splitLineArray = (inputArray[i]).split(' ');
+        numCount[i-1] = splitLineArray[0];
+        numSymbols[i-1] = splitLineArray[1];
+    }
+
+    for(var j=0; j<numCount.length;j++)
+    {
+        solutionString += "Case #" + String(j +1) + ": ";
+        for(var k=0;k<Number(numCount[j]);k++)
+        {
+            solutionString += String(numSymbols[j]);
+        }
+        if((j+1) != numCount.length)
+        {
+            solutionString += "\n";
+        }
+    }
+
+    return solutionString;
+}
