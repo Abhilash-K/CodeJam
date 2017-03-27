97399e4be3fa08d6b921d44ced304b3755d6e267
Abhilash K <abhilash.face@gmail.com>
2017-03-27 22:34:38 +0530

    Started revenge of the pancakes
    https://code.google.com/codejam/contest/6254486/dashboard

---
+++ Revenge of the Pancakes/solution.js
@@ -0,0 +1,38 @@
+/**
+ * Created by Abhilash K on 3/26/2017.
+ */
+
+function findSolutionOld(inputArray)
+{
+    
+}
+
+function findSolutionOld(inputArray)
+{
+    var numtestCases = inputArray[0];
+    var numCount = [];
+    var numSymbols = [];
+    var splitLineArray = [];
+    var solutionString = "";
+    for(var i=1;i<=numtestCases;i++)
+    {
+        splitLineArray = (inputArray[i]).split(' ');
+        numCount[i-1] = splitLineArray[0];
+        numSymbols[i-1] = splitLineArray[1];
+    }
+
+    for(var j=0; j<numCount.length;j++)
+    {
+        solutionString += "Case #" + String(j +1) + ": ";
+        for(var k=0;k<Number(numCount[j]);k++)
+        {
+            solutionString += String(numSymbols[j]);
+        }
+        if((j+1) != numCount.length)
+        {
+            solutionString += "\n";
+        }
+    }
+
+    return solutionString;
+}
