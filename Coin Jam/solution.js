110a4ebc338ae234bb72c5b686441a723f399c31
Abhilash K <abhilash.face@gmail.com>
2017-03-29 12:32:49 +0530

    step 1

--- Coin Jam/solution.js
+++ Coin Jam/solution.js
@@ -5,19 +5,33 @@
 function findSolution(inputArray)
 {
     var numtestCases = inputArray[0];
+    var T = [];
+    var splitT = [];
+    var numDigits = [];
+    var currentRequiredDigits = 0;
+    var numTestCases = [];
+    var currentTestCases = 0;
     var solutionString = "";
 
     for(var i=1;i<=numtestCases;i++)
     {
-        S[i-1] = inputArray[i];
+        T[i-1] = inputArray[i];
+        splitT = String(inputArray[i]).split(" ");
+        numDigits[i-1] = splitT[0];
+        numTestCases[i-1] = splitT[1];
     }
-    for(var j=0; j<S.length;j++)
+
+    for(var j=0; j<T.length;j++)
     {
-        solutionString += "Case #" + String(j +1) + ": ";
+        solutionString += "Case #" + String(j +1) + ":\n";
+        currentRequiredDigits = numDigits[i];
+        currentTestCases = numTestCases[i];
+        for(var k=0;k<currentTestCases;k++)
+        {
 
 
-
-        if((j+1) != S.length)
+        }
+        if((j+1) != T.length)
         {
             solutionString += "\n";
         }
