a6f1aac4bf454af0a1ef00c2c1d9a224b26830ee
Abhilash K <abhilash.face@gmail.com>
2017-03-29 16:55:33 +0530

    step 2

--- Coin Jam/solution.js
+++ Coin Jam/solution.js
@@ -12,6 +12,11 @@ function findSolution(inputArray)
     var numTestCases = [];
     var currentTestCases = 0;
     var solutionString = "";
+    var isPrime = true;
+    var isJamCoin = false;
+    var checkedNumLim = 2;
+    var checkedNumLimParsed = 2;
+    var currentBinary = "";
 
     for(var i=1;i<=numtestCases;i++)
     {
@@ -28,7 +33,33 @@ function findSolution(inputArray)
         currentTestCases = numTestCases[i];
         for(var k=0;k<currentTestCases;k++)
         {
-
+            isJamCoin = false;
+            checkedNumLim = 2;
+            while (!isJamCoin)
+            {
+                while (isPrime)
+                {
+                    checkedNumLim++;
+                    for (var l = 2; l <= checkedNumLim / 2; l++)
+                    {
+                        if (checkedNumLim % i == 0)
+                        {
+                            isPrime = false;
+                            break;
+                        }
+                    }
+                }
+                checkedNumLimParsed = parseInt(checkedNumLim);
+                currentBinary = checkedNumLim.toString(2);
+                if(currentBinary[0] != "1")
+                {
+                    continue;
+                }
+                if(currentBinary[(currentBinary.length - 1)] != "1")
+                {
+                    continue;
+                }
+            }
 
         }
         if((j+1) != T.length)
