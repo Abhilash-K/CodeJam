f0acd5fd627c2ad00641ed9acaadaa5e4c065cba
Abhilash K <abhilash.face@gmail.com>
2017-03-31 17:03:34 +0530

    step 5

--- Coin Jam/solution.js
+++ Coin Jam/solution.js
@@ -17,7 +17,11 @@ function findSolution(inputArray)
     var checkedNumLim = 2;
     var checkedNumLimParsed = 2;
     var currentBinary = "";
-    var baseMultiplier = 1;
+    var currentMultiplier = 1;
+    var sumOfBinary = 0;
+    var falsePositive = true;
+    var listOfDivisors = [];
+    var currentDivisor = 0;
 
     for(var i=1;i<=numtestCases;i++)
     {
@@ -30,29 +34,44 @@ function findSolution(inputArray)
     for(var j=0; j<T.length;j++)
     {
         solutionString += "Case #" + String(j +1) + ":\n";
-        currentRequiredDigits = numDigits[i];
-        currentTestCases = numTestCases[i];
+        currentRequiredDigits = Number(numDigits[j]);
+        currentTestCases = Number(numTestCases[j]);
+        checkedNumLim = 2;
         for(var k=0;k<currentTestCases;k++)
         {
             isJamCoin = false;
-            checkedNumLim = 2;
             while (!isJamCoin)
             {
+                listOfDivisors = [];
                 isPrime = true;
                 while (isPrime)
                 {
                     checkedNumLim++;
+                    currentDivisor = 0;
                     for (var l = 2; l <= checkedNumLim / 2; l++)
                     {
-                        if (checkedNumLim % i == 0)
+                        if (checkedNumLim % l == 0)
                         {
                             isPrime = false;
+                            if(currentDivisor == 0)
+                            {
+                                currentDivisor = l;
+                            }
                             break;
                         }
                     }
                 }
+                listOfDivisors.push(currentDivisor);
                 checkedNumLimParsed = parseInt(checkedNumLim);
                 currentBinary = checkedNumLim.toString(2);
+                if(currentBinary.length > currentRequiredDigits)
+                {
+                    return "0";
+                }
+                if(currentBinary.length != currentRequiredDigits)
+                {
+                    continue;
+                }
                 if(currentBinary[0] != "1")
                 {
                     continue;
@@ -61,14 +80,66 @@ function findSolution(inputArray)
                 {
                     continue;
                 }
-                for(var m = 3; m <= 10; m++)
-                {
-                    baseMultiplier = 1;
-                    while()
-                    {
 
+                for (var m = 3; m <= 10; m++)
+                {
+                    falsePositive = true;
+                    sumOfBinary = 0;
+                    for (var n = 0; n < currentBinary.length; n++)
+                    {
+                        currentMultiplier = Math.pow(m, n);
+                        sumOfBinary += currentBinary[n] * currentMultiplier;
                     }
+                    currentDivisor = 0;
+                    for (var p = 2; p <= Math.sqrt(sumOfBinary); p++)
+                    {
+                        if (sumOfBinary % p == 0)
+                        {
+                            falsePositive = false;
+                            if(currentDivisor == 0)
+                            {
+                                currentDivisor = p;
+                            }
+                            break;
+                        }
+                    }
+                    listOfDivisors.push(currentDivisor);
+                    if(falsePositive == true)
+                    {
+                        break;
+                    }
+                    /*if(currentDivisor == 0)
+                    {
+                        console.log("error");
+                        for (var p = 2; p <= Math.sqrt(sumOfBinary); p++)
+                        {
+                            if (sumOfBinary % p == 0)
+                            {
+                                falsePositive = false;
+                                if(currentDivisor == 0)
+                                {
+                                    currentDivisor = p;
+                                }
+                                break;
+                            }
+                        }
+                    }*/
                 }
+                if(falsePositive == false)
+                {
+                    solutionString += currentBinary;
+                    for(var o = 0; o<listOfDivisors.length;o++)
+                    {
+                        solutionString += " " + listOfDivisors[o];
+                    }
+                    if(k+1 != currentTestCases)
+                    {
+                        solutionString += "\n";
+                    }
+
+                    isJamCoin = true;
+                }
+
             }
 
         }
